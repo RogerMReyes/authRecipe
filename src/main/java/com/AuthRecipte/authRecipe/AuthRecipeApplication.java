56f8e950d8d69e84f400e16a7786f70d4c6e87bc
RogerMReyes <Rogerrmr14@gmail.com>
2022-06-06 14:35:15 -0700

    Added base files

---
+++ src/main/java/com/AuthRecipte/authRecipe/AuthRecipeApplication.java
@@ -0,0 +1,13 @@
+package com.AuthRecipte.authRecipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthRecipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthRecipeApplication.class, args);
+	}
+
+}
